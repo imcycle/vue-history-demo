eae164f7c9c75046cd47ab00535028948201afa3
曹宇聪 <caoyucong@caoyucongdeMBP.lan>
2023-03-07 16:44:59 +0800

    first commit

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+import { createRouter, createWebHistory, createWebHashHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHashHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      component: () => import('../views/AboutView.vue')
+    },
+    {
+      path: '/user',
+      name: 'user',
+      component: () => import('../views/UserView.vue')
+    },
+    {
+      path: '/center',
+      name: 'center',
+      component: () => import('../views/CenterView.vue')
+    }
+  ]
+})
+
+export default router
